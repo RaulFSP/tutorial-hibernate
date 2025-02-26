51536972086d1ef361234d0b72f47f22d0e9c065
raul <raulfsp@protonmail.com>
2025-02-26 16:47:22 -0400

    first

---
+++ src/test/java/noob/tutorial/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package noob.tutorial.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
